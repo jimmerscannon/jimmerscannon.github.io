502e1ff0129e68bdfdbc28d705ed7ba34ab7fab8
aappel1 <austin.a360@gmail.com>
2016-06-05 11:25:51 -0400

    added male part

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-var possible = ["Andres", "Austen", "Austin", "Chase", "Jimmy", "Nate", "Nick" ];
+var possible = [];
 var matchFound = false;
 var question;
 
-//--------------------Question Sets--------------------
-
+//--------------------Question Sets(female)--------------------
 function question1() {
 	question = confirm("Do you like tall men?\n(Ok for yes, Cancel for no)");
 	if (question) {
@@ -61,6 +60,68 @@ function question6() {
 //------------------------------------------------------
 
 
+//--------------------Question Sets(male)--------------------
+
+
+function fquestion1() {
+	question = confirm("Do you like adventurous women?\n(Ok for yes, Cancel for no)");
+	if (question) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function fquestion2() {
+	question = confirm("Do you enjoy killing the British?\n(Ok for yes, Cancel for no)");
+	if (question) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function fquestion3() {
+	question = confirm("Do you like women who work desk jobs?\n(Ok for yes, Cancel for no)");
+	if (question) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function fquestion4() {
+	question = confirm("Do you like transformers?\n(Ok for yes, Cancel for no)");
+	if (question) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function fquestion5() {
+	question = confirm("Do you ACTUALLY love your wife Jackie?\n(Ok for yes, Cancel for no)");
+	if (question) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function fquestion6() {
+	question = confirm("Given the choice, would you change your name to John or Bill?\n(Ok for John, Cancel for Bill)");
+	if (question) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+
+//-----------------------------------------------------------
+
+
 //---------------------BS Questions---------------------
 
 var bsq = ["What is your favorite color", "How many hours a day do you use the internet", "What is your favorite sport", "What is your favorite movie?",
@@ -86,10 +147,12 @@ function askBsq() {
 
 //------------------------------------------------------
 
-var name = prompt("What is your name?");
-var age = prompt("What is your age?");
-var gender = prompt("What is your gender?");
+//var name = prompt("What is your name?");
+//var age = prompt("What is your age?");
+var gender = prompt("What is your gender?\n(male or female)");
 
+if (gender.toLowerCase() === "female") {  //Female storyline (ADD STORY!!!!!!!!!!!!!)
+possible = ["Andres", "Austen", "Austin", "Chase", "Jimmy", "Nate", "Nick" ];
 
 //Do you like tall men?
 if (question1()) {
@@ -168,6 +231,117 @@ if(question5()) {
 }
 
 
+
+
+printPossible();
+displayImage();
+
+
+} else {   //Add male storyline
+var match = "";
+matchFound = false;
+possible = ["joan", "saca", "monica", "trans", "marilyn"];
+
+
+if (fquestion1()) {
+	if (fquestion2()) {
+		match = "joan"
+		matchFound = true;
+	} else {
+		if (fquestion4()) {
+			match = "trans";
+			matchFound = true;
+		} else {
+			if(fquestion3()) {
+				match = "monica";
+				matchFound = true;
+			} else {
+				match = "saca";
+				matchFound = true;
+			}
+		}
+	}
+} else {
+
+if (fquestion5()) {
+	alert("LIAR");
+	if (fquestion6()) {
+		if (!matchFound) {
+			match="marilyn"
+		}
+	} else {
+		if (!matchFound) {
+			match = "monica"
+		}
+	}
+} else {
+	if (fquestion6()) {
+		if (!matchFound) {
+			match="marilyn"
+		}
+	} else {
+		if (!matchFound) {
+			match = "monica"
+		}
+	}
+}
+
+}
+
+
+switch(match) {
+	case "joan":
+		document.getElementById("s1").innerHTML = "Joan of Arc";
+		var image = document.getElementById("person");
+		image.src = ("img/img_"+match+".JPG");
+		break;
+	case "trans":
+		document.getElementById("s1").innerHTML = "Transformer Girl";
+		var image = document.getElementById("person");
+		image.src = ("img/img_"+match+".JPG");
+		break;
+	case "saca":
+		document.getElementById("s1").innerHTML = "Sacagawea";
+		var image = document.getElementById("person");
+		image.src = ("img/img_"+match+".JPG");
+		break;
+	case "monica":
+		document.getElementById("s1").innerHTML = "Monica Lewinsky";
+		var image = document.getElementById("person");
+		image.src = ("img/img_"+match+".JPG");
+		break;
+	case "marilyn":
+		document.getElementById("s1").innerHTML = "Marilyn Monroe";
+		var image = document.getElementById("person");
+		image.src = ("img/img_"+match+".JPG");
+		break;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+printMatch();
+
+}
+
+function printMatch() {
+	console.log(match);
+}
+
+function consolePossible() {
+	for (var i = 0; i < possible.length; i++) {
+		console.log(possible[i]);
+	}
+}
+
 function printPossible() {
 	for (var i = 0; i < possible.length; i++) {
 		console.log(possible[i]);
@@ -180,5 +354,3 @@ function displayImage() {
 	image.src = ("img/img_"+possible[0].toLowerCase()+".JPG");
 }
 
-printPossible();
-displayImage();
